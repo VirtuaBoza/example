c79cedaf522167c3156aebaa6457f21d8bdb94dc
Andrew Boza <aboza@madmobile.com>
2020-10-14 18:11:13 -0400

    tweaks

--- new/withContext.js
+++ new/withContext.js
@@ -1,18 +1,22 @@
 import React, { useContext } from "react";
 import compose from "../compose";
 
-// takes a context map and returns a function which takes a component and returns a component
+/**
+ * Takes a context map (ex. { configStore: ConfigStoreContext })
+ * and returns a HOC which is supplied with props
+ * with the map's keys and relative Context values.
+ */
 export default function withContext(contextMap) {
-  return (WrappedComponent) =>
+  return (component) =>
     compose(
       ...Object.entries(contextMap).map(([key, Context]) => {
-        return (InnerComponent) => {
+        return (Component) => {
           return React.forwardRef((props, ref) => {
-            const context = useContext(Context);
-            const contextProp = { [key]: context };
-            return <InnerComponent ref={ref} {...contextProp} {...props} />;
+            const contextValue = useContext(Context);
+            const contextProp = { [key]: contextValue };
+            return <Component ref={ref} {...contextProp} {...props} />;
           });
         };
       })
-    )(WrappedComponent);
+    )(component);
 }
